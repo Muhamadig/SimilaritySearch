23c300619c81501c72c5107492b01bee5ca53e22
abdalla srouji <abdalla944@gmail.com>
2017-04-08 00:46:20 +0300

    no message

--- DBPreProcessing/src/controller/ReadFile.java
+++ DBPreProcessing/src/controller/ReadFile.java
@@ -1,6 +1,7 @@
 package controller;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
@@ -25,4 +26,50 @@ public class ReadFile {
 		System.out.println();
 		return text;
 	}
+	
+		public static String docConverter(File input) throws IOException
+	{
+			String text = null;
+	        FileInputStream fis;
+	     		try {
+				fis = new FileInputStream(input.getAbsolutePath());
+				 HWPFDocument document = new HWPFDocument(fis);
+				 text = new WordExtractor(document).getText();
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return text;
+	}
+		public static String docxConverter (File input) throws IOException
+		{
+			String text =null;
+			  FileInputStream fis;
+			   XWPFWordExtractor ex = null;
+			   XWPFDocument DOC;
+			  try{
+				  fis = new FileInputStream(input.getAbsolutePath());
+				  DOC = new XWPFDocument(fis);
+				  ex = new XWPFWordExtractor(DOC);
+				  text = ex.getText();
+			  }catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			return text;
+		}
+		public static String pdfConverter (File input)
+		{
+			String text =null;
+			 try {
+		            PDDocument doc = PDDocument.load(input);
+		        text = new PDFTextStripper().getText(doc);
+		           
+		        } catch (IOException e) {
+		            // TODO Auto-generated catch block
+		            e.printStackTrace();
+		        } 
+			 return text;
+		}
+		
 }
