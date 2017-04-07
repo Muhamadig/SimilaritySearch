adfcdbfd22e5038f4aad2626c3c92bfeaee850ee
Muhamad Igbaria <muhamadig.ac@gmail.com>
2017-04-08 00:44:28 +0300

    readfiles class

--- DBPreProcessing/src/controller/ReadFile.java
+++ DBPreProcessing/src/controller/ReadFile.java
@@ -2,6 +2,7 @@ package controller;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
@@ -12,7 +13,6 @@ public class ReadFile {
 	public static String pdfRead(String fileDest){
 		File file = new File(fileDest); 
 		String text=null;
-		int flag=1;
 		try {
 			PDDocument document = PDDocument.load(file);
 			PDFTextStripper pdfStripper = new PDFTextStripper();
@@ -21,8 +21,34 @@ public class ReadFile {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		System.out.println(text);
-		System.out.println();
 		return text;
+	}
+	
+	public static String docxRead(String fileDest){
+		return null;
+	}
+	
+	public static HashMap<String, Integer> textToFrequency (String text){
+		
+		HashMap<String, Integer> freq=new HashMap<>();
+		
+		String [] textWords=text.trim().split(" ");
+		String [] words=filter(textWords);
+		System.out.println("words in string array=" + words.length);
+		for(String str:words){
+			
+			if(freq.containsKey(str)) freq.replace(str, freq.get(str),freq.get(str)+1 );
+			else freq.put(str, 1);
+		}
+		freq.remove("");
+		return freq;
+	}
+	
+	public static String[] filter (String [] words)
+	{
+		int len= words.length;
+		for(int i=0;i<len;i++)
+			words[i]=words[i].replaceAll("[^a-zA-Z0-9']","");
+		return words;
 	}
 }
