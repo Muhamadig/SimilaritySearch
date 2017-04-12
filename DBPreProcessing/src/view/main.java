3bf9e077731780e3ac9763bda2847505ba3fbf13
Muhamad Igbaria <muhamadig.ac@gmail.com>
2017-04-12 22:09:24 +0300

    controller.Stemmer packages added ...

--- DBPreProcessing/src/view/main.java
+++ DBPreProcessing/src/view/main.java
@@ -1,13 +1,17 @@
 package view;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.rmi.PortableRemoteObject;
+
 import controller.StopWordsFiltering;
 import controller.ReadFile;
 import controller.Stemmer;
+import controller.stemmer.snowball.*;
+import controller.stemmer.snowball.SnowballStemmer.ALGORITHM;
 import controller.Util;
+import controller.stemmer.PorterStemmer;
 import model.FrequencyVector;
 
 public class main {
@@ -17,9 +21,11 @@ public class main {
 		
 		FrequencyVector init_words= ReadFile.ReadFile("PDFs/file1_EN.pdf", "pdf");
 		FrequencyVector swf_words=StopWordsFiltering.RemoveSW(init_words, "english");
+		PorterStemmer stm= new PorterStemmer();
+		System.out.println(stm.stem("explosion"));
+		SnowballStemmer sns=new SnowballStemmer(ALGORITHM.ENGLISH);
+		System.out.println(sns.stem("working"));
 		
-		
-
 
 	}
 
