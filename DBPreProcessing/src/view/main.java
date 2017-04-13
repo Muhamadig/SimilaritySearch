0316cd5eb6e95c870d3b062a90d0a6b0d9d37a1b
Muhamad Igbaria <muhamadig.ac@gmail.com>
2017-04-13 22:04:01 +0300

    Opennlp framework added to the project as jars

--- DBPreProcessing/src/view/main.java
+++ DBPreProcessing/src/view/main.java
@@ -1,9 +1,11 @@
 package view;
 import controller.ReadFile;
+import opennlp.tools.*;
+import opennlp.tools.stemmer.PorterStemmer;
+import opennlp.tools.stemmer.snowball.SnowballStemmer;
+import opennlp.tools.stemmer.snowball.SnowballStemmer.ALGORITHM;
 import controller.StopWordsFiltering;
-import controller.stemmer.PorterStemmer;
-import controller.stemmer.snowball.SnowballStemmer;
-import controller.stemmer.snowball.SnowballStemmer.ALGORITHM;
+
 import model.FrequencyVector;
 import org.apache.lucene.analysis.en.*;
 public class main {
@@ -13,12 +15,11 @@ public class main {
 		
 		FrequencyVector init_words= ReadFile.ReadFile("PDFs/file1_EN.pdf", "pdf");
 		FrequencyVector swf_words=StopWordsFiltering.RemoveSW(init_words, "english");
-		PorterStemmer stm= new PorterStemmer();
-		System.out.println(stm.stem("funniest"));
+PorterStemmer ss= new PorterStemmer();
+		System.out.println(ss.stem("funniest"));
 		SnowballStemmer sns=new SnowballStemmer(ALGORITHM.ENGLISH);
 		System.out.println(sns.stem("funniest"));
 		System.err.println();
-		EnglishAnalyzer en=new EnglishAnalyzer();
 		
 		
 		
