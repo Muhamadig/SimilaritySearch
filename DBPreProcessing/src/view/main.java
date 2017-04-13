ad60fc40ff8c4bb32a5811594dacfb2dd05938e2
abdalla srouji <abdalla944@gmail.com>
2017-04-13 22:23:49 +0300

    no message

--- DBPreProcessing/src/view/main.java
+++ DBPreProcessing/src/view/main.java
@@ -11,19 +11,12 @@ import org.apache.lucene.analysis.en.*;
 public class main {
 
 	public static void main(String[] args) {
-		
-		
 		FrequencyVector init_words= ReadFile.ReadFile("PDFs/file1_EN.pdf", "pdf");
 		FrequencyVector swf_words=StopWordsFiltering.RemoveSW(init_words, "english");
-PorterStemmer ss= new PorterStemmer();
+		PorterStemmer ss= new PorterStemmer();
 		System.out.println(ss.stem("funniest"));
 		SnowballStemmer sns=new SnowballStemmer(ALGORITHM.ENGLISH);
 		System.out.println(sns.stem("funniest"));
-		System.err.println();
-		
-		
-		
-
 	}
 
 }
