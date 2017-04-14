404dd6dab06ddb009704ab2aace04026fba4216c
Muhamad Igbaria <muhamadig.ac@gmail.com>
2017-04-14 04:45:42 +0300

    no message

--- DBPreProcessing/src/view/main.java
+++ DBPreProcessing/src/view/main.java
@@ -1,16 +1,11 @@
 package view;
-import controller.ReadFile;
-import opennlp.tools.*;
-import opennlp.tools.stemmer.PorterStemmer;
-import opennlp.tools.stemmer.snowball.SnowballStemmer;
-import opennlp.tools.stemmer.snowball.SnowballStemmer.ALGORITHM;
-import controller.StopWordsFiltering;
 
-import model.FrequencyVector;
-import org.apache.lucene.analysis.en.*;
+
+
 public class main {
 
 	public static void main(String[] args) {
+	
+		
 	}
-
 }
