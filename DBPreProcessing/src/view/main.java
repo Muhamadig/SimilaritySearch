75a0ea731856fdeb91425418cddade03a0e9e4bf
abdalla srouji <abdalla944@gmail.com>
2017-04-14 12:30:02 +0300

    trying steaming alogrithm (failed)

--- DBPreProcessing/src/view/main.java
+++ DBPreProcessing/src/view/main.java
@@ -1,11 +1,24 @@
 package view;
 
+import org.tartarus.snowball.ext.PorterStemmer;
+
+import controller.Stemmer;
+
 
 
 public class main {
 
 	public static void main(String[] args) {
 	
-		
+		PorterStemmer stemmer = new PorterStemmer();
+		stemmer.setCurrent("apples"); //set string you need to stem
+		stemmer.stem();  //stem the word
+		System.out.println(stemmer.getCurrent());//get the stemmed word
+		Stemmer stm = new Stemmer();
+		String str = "apples";
+		stm.add(str.toCharArray(), str.length());
+		stm.stem();
+		System.out.println(stm.toString());
 	}
 }
+
