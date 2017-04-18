3e8cf8552524a1fc16f368e6cbe54d9b6ed0ea8f
Muhamad Igbaria <muhamadig.ac@gmail.com>
2017-04-18 04:38:35 +0300

    no message

--- DBPreProcessing/src/view/main.java
+++ DBPreProcessing/src/view/main.java
@@ -1,8 +1,27 @@
 package view;
+
+import java.io.StringReader;
+
+import org.apache.lucene.analysis.*;
+import org.apache.lucene.analysis.standard.*;
+import org.apache.lucene.analysis.tokenattributes.TermToBytesRefAttribute;
+import org.apache.lucene.util.Version;
 public class main {
 
 	public static void main(String[] args) {
 		
+		 String text 
+	      = "Lucene is simple yet powerful java based search library.";
+	   Analyzer analyzer = new StandardAnalyzer();
+	   TokenStream tokenStream 
+	      = analyzer.tokenStream("contents",
+	        new StringReader(text));
+	   
+	   tokenStream.
+	   TermToBytesRefAttribute term = ((Object) tokenStream).addAttribute(TermToBytesRefAttribute.class);
+	   while(((Object) tokenStream).incrementToken()) {
+	      System.out.print("[" + term.term() + "] ");
+	   }
 	}
 }
 
