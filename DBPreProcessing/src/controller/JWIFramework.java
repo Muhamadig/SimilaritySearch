28bb61884894509b55b6d838cf660a6e4a07b57f
Muhamad Igbaria <muhamadig.ac@gmail.com>
2017-05-03 22:21:29 +0300

    no message

--- DBPreProcessing/src/controller/JWIFramework.java
+++ DBPreProcessing/src/controller/JWIFramework.java
@@ -17,7 +17,9 @@ import edu.mit.jwi.item.IWord;
 import edu.mit.jwi.item.IWordID;
 import edu.mit.jwi.item.POS;
 import edu.mit.jwi.item.Word;
+import edu.mit.jwi.item.WordID;
 import edu.mit.jwi.morph.WordnetStemmer;
+import model.FVHashMap;
 
 public class JWIFramework {
 
@@ -65,33 +67,44 @@ public class JWIFramework {
 
 	}
 
-	public static HashSet<String> getSynonyms ( IDictionary dict ,String key,POS pos){
+	public static HashSet<String> getSynonyms ( IDictionary dict ,String key,POS pos,FVHashMap fv){
 
 		HashSet<String> set=new HashSet<>();
 		// look up first sense of the word "dog "
 		IIndexWord idxWord = dict.getIndexWord(key, pos);
 		if(idxWord==null) return null;
-		IWordID wordID = idxWord . getWordIDs ().get (0) ; // 1st meaning
-		IWord word = dict . getWord ( wordID );
-		ISynset synset = word . getSynset ();
-
-		// iterate over words associated with the synset
-		for( IWord w : synset . getWords ())
-			set.add(w.getLemma());
+//		List<IWordID> wordids=idxWord . getWordIDs ();
+//		for(IWordID wordID:wordids){
+//			IWord word = dict . getWord ( wordID );
+//			ISynset synset = word . getSynset ();
+//
+//			// iterate over words associated with the synset
+//			String tmp_lema;
+//			for(IWord w : synset . getWords ())
+//				if(fv.containsKey(tmp_lema=w.getLemma())) set.add(tmp_lema);
+//		}
+				IWordID wordID = idxWord . getWordIDs ().get (0) ; // 1st meaning
+				IWord word = dict . getWord ( wordID );
+				ISynset synset = word . getSynset ();
 		
+				// iterate over words associated with the synset
+				String tmp_lema;
+				for( IWord w : synset . getWords ())
+					if(fv.containsKey(tmp_lema=w.getLemma())) set.add(tmp_lema);
+
 		return set;
 	}
-	
-	public static HashSet<String> getAllSynonyms(IDictionary dict ,String key){
-		
+
+	public static HashSet<String> getAllSynonyms(IDictionary dict ,String key,FVHashMap fv){
+
 		HashSet<String> set=new HashSet<>();
 		HashSet<String> temp;
-		if((temp = getSynonyms(dict, key, POS.ADJECTIVE))!=null) set.addAll(temp);
-		if((temp = getSynonyms(dict, key, POS.ADVERB))!=null) set.addAll(temp);
-		if((temp = getSynonyms(dict, key, POS.NOUN))!=null) set.addAll(temp);
-		if((temp = getSynonyms(dict, key, POS.VERB))!=null) set.addAll(temp);
+		if((temp = getSynonyms(dict, key, POS.ADJECTIVE,fv))!=null) set.addAll(temp);
+		if((temp = getSynonyms(dict, key, POS.ADVERB,fv))!=null) set.addAll(temp);
+		if((temp = getSynonyms(dict, key, POS.NOUN,fv))!=null) set.addAll(temp);
+		if((temp = getSynonyms(dict, key, POS.VERB,fv))!=null) set.addAll(temp);
 		return set;
-		
-		
+
+
 	}
 }
