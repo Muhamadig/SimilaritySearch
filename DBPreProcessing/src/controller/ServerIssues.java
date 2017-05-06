5b8568bb85fa9d96c78b0bb0d2dce4cbae3e3b48
abdalla srouji <abdalla944@gmail.com>
2017-05-06 10:43:20 +0300

    improve the collectsimilar function look at the result3.txt file

--- DBPreProcessing/src/controller/ServerIssues.java
+++ DBPreProcessing/src/controller/ServerIssues.java
@@ -12,14 +12,15 @@ import edu.mit.jwi.item.IWord;
 
 import java.util.Iterator;
 import java.util.Set;
+import java.util.TreeMap;
 public class ServerIssues {
 
 	private IDictionary dictionary;
-	private HashMap<String, HashSet<String>> synonyms;
+	private TreeMap<String, HashSet<String>> synonyms;
 	private HashMap<HashSet<String>,HashSet<String>> similars;
 	public ServerIssues(IDictionary dic){
 		this.dictionary = dic;
-		synonyms = new HashMap<String,HashSet<String>>();
+		synonyms = new TreeMap<String,HashSet<String>>();
 		similars = new HashMap<HashSet<String>,HashSet<String>>();
 		}
 	
@@ -41,7 +42,7 @@ public class ServerIssues {
 			writer.println(Stem +": "+ synonyms.get(Stem));
 			}
 		}
-		System.out.println("done "+(System.currentTimeMillis()-t));
+		System.out.println("done ..."+(System.currentTimeMillis()-t));
 		    writer.close();
 		} catch (IOException e) {
 		   // do something
@@ -56,42 +57,50 @@ public class ServerIssues {
 		PrintWriter writer = new PrintWriter("EnglishWordsList/results2.txt","UTF-8");
 		for(String word : synonyms.keySet()){
 			HashSet<String> syns = synonyms.get(word);
-			Iterator<String> it = syns.iterator();
-			while(it.hasNext()){
-				String syn = it.next();
-				if(synonyms.containsKey(syn))
-					if(!synonyms.get(syn).contains(word))
-						synonyms.get(syn).add(word);
+			if(syns.isEmpty())
+				syns.add(word);
+			else{
+				Iterator<String> it = syns.iterator();
+				while(it.hasNext()){
+					String syn = it.next();
+					if(synonyms.containsKey(syn))
+						if(!synonyms.get(syn).contains(word))
+							synonyms.get(syn).add(word);
+				}
 			}
+			
 			writer.println(word +": " + synonyms.get(word));
 		}
-		System.out.println("done "+(System.currentTimeMillis()-t));
+		System.out.println("done ..."+(System.currentTimeMillis()-t));
 	    writer.close();
 	}
 	
 	public void CollectSimilars() throws FileNotFoundException, UnsupportedEncodingException{
 		System.out.print ("\n Similars ... \n");
 		long t = System . currentTimeMillis ();
-		HashMap<String, HashSet<String>> copy = (HashMap<String, HashSet<String>>) synonyms.clone();
+		TreeMap<String, HashSet<String>> copy = (TreeMap<String, HashSet<String>>) synonyms.clone();
 		HashMap<String,Boolean> checkList = new HashMap<String,Boolean>();
 		for(String key:copy.keySet()){
 			checkList.put(key, false);
 		}
+		HashSet<String> values;
 		HashSet<String> keys = new HashSet<String>();
 		PrintWriter writer = new PrintWriter("EnglishWordsList/results3.txt","UTF-8");
-		for(String key : copy.keySet()){
-			HashSet<String> values = copy.get(key);
-			for(String key1 : copy.keySet())
-				if(!checkList.get(key1) && values.equals(copy.get(key1)))
-				{
+		Set<String> k = copy.keySet();
+		for(String key: k){
+			values = copy.get(key);
+			for(String key1 : k){
+				if(!checkList.get(key1) && values.equals(copy.get(key1))){
+					checkList.replace(key1, false, true);
 					keys.add(key1);
-					checkList.replace(key1, true);
 				}
+			}
 			similars.put(keys, values);
 			writer.println(keys +": " + values);
-			keys.removeAll(keys);
+			keys.clear();
 		}
-		System.out.println("done "+(System.currentTimeMillis()-t));
+		System.out.println("done ..."+(System.currentTimeMillis()-t));
 		writer.close();
 	}
+	
 }
