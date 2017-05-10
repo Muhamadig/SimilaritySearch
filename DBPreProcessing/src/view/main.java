796b5ac92ab370dc5c2d98824d1345fc87e77b1b
abdalla srouji <abdalla944@gmail.com>
2017-05-11 00:38:22 +0300

    no message

--- DBPreProcessing/src/view/main.java
+++ DBPreProcessing/src/view/main.java
@@ -1,13 +1,10 @@
 package view;
 
 
-import java.io.IOException;
 
-import edu.mit.jwi.IDictionary;
-import net.sf.extjwnl.JWNLException;
 public class main {
 
-	public static void main(String[] args) throws IOException, JWNLException {
+	public static void main(String[] args){
 	System.out.println();
 	}
 }
