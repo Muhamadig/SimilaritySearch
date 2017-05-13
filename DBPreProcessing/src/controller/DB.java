15ec4ab9f65707fb71088f888cd74db16a6b26c8
abdalla srouji <abdalla944@gmail.com>
2017-05-13 10:20:28 +0300

    no message

--- DBPreProcessing/src/controller/DB.java
+++ DBPreProcessing/src/controller/DB.java
@@ -1,5 +1,12 @@
 package controller;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -18,22 +25,47 @@ import model.Language.Langs;
 public class DB {
 
 	private int NOtexts;
-	private String url = "http://www.courts.ie/Judgments.nsf/frmJudgmentsByYearAll?OpenForm&amp;Start=1&amp;Count=30&amp;CollapseView&amp;Seq=6";
 	private ArrayList<String> links;
 	private ArrayList <String> texts;
 	private HashMap<String , FVHashMap> freqvec; 
+	private final String [] pages ={
+			"http://www.courts.ie/Judgments.nsf/frmJudgmentsByYearAll?OpenForm&ExpandView&Seq=1",
+			"http://www.courts.ie/Judgments.nsf/frmJudgmentsByYearAll?OpenForm&Start=1.1.28&ExpandView&Seq=2",
+			"http://www.courts.ie/Judgments.nsf/frmJudgmentsByYearAll?OpenForm&Start=1.1.57&ExpandView&Seq=3", 
+			"http://www.courts.ie/Judgments.nsf/frmJudgmentsByYearAll?OpenForm&Start=1.1.86&ExpandView&Seq=4",
+			"http://www.courts.ie/Judgments.nsf/frmJudgmentsByYearAll?OpenForm&Start=1.1.115&ExpandView&Seq=5",
+			"http://www.courts.ie/Judgments.nsf/frmJudgmentsByYearAll?OpenForm&Start=1.1.115&ExpandView&Seq=6",
+			"http://www.courts.ie/Judgments.nsf/frmJudgmentsByYearAll?OpenForm&Start=1.3.1&ExpandView&Seq=7",
+			"http://www.courts.ie/Judgments.nsf/frmJudgmentsByYearAll?OpenForm&Start=1.3.30&ExpandView&Seq=8",
+			"http://www.courts.ie/Judgments.nsf/frmJudgmentsByYearAll?OpenForm&Start=1.3.59&ExpandView&Seq=9",
+			"http://www.courts.ie/Judgments.nsf/frmJudgmentsByYearAll?OpenForm&Start=1.3.88&ExpandView&Seq=10",
+			"http://www.courts.ie/Judgments.nsf/frmJudgmentsByYearAll?OpenForm&Start=1.3.117&ExpandView&Seq=11",
+			"http://www.courts.ie/Judgments.nsf/frmJudgmentsByYearAll?OpenForm&Start=1.3.146&ExpandView&Seq=12",
+			"http://www.courts.ie/Judgments.nsf/frmJudgmentsByYearAll?OpenForm&Start=1.3.175&ExpandView&Seq=13",
+			"http://www.courts.ie/Judgments.nsf/frmJudgmentsByYearAll?OpenForm&Start=1.3.204&ExpandView&Seq=14",
+			"http://www.courts.ie/Judgments.nsf/frmJudgmentsByYearAll?OpenForm&Start=1.4.9&ExpandView&Seq=15"};
 	public DB(){
 		texts = new ArrayList<String>();
 		links = new ArrayList<String>();
 		freqvec = new HashMap <String,FVHashMap>();
+		
 	}
 	
-	
+
+	public void LoadLinks(){
+		for(String link : pages){
+			try {
+				getLinks(link);
+			} catch (IOException e) {
+				System.out.println("Bad Link");
+			}
+		}
+	}
 	
 	public void init() throws IOException{
 		long t = System.currentTimeMillis();
-		System.out.println("Getting Texts..\n");
-		getLinks();
+		System.out.println("Initialize...");
+		LoadLinks();
 		Iterator<String> it = links.iterator();
 		while(it.hasNext()){
 			getText(it.next());
@@ -41,12 +73,7 @@ public class DB {
 		}
 		System.out.println("Done ... "+(System.currentTimeMillis()-t));
 	}
-	public void GetContent() throws IOException{
-		Document document = Jsoup.connect("http://www.courts.ie/Judgments.nsf/597645521f07ac9a80256ef30048ca52/8e8929c277c2fe5e8025811a0057b95a?OpenDocument").followRedirects(false).timeout(60000/*wait up to 60 sec for response*/).get();
-		String value = document.body().getElementById("ContentsFrame").text();
-		value = ReadFile.clean(value);
-		texts.add(value);
-	}
+	
 	public String getTitle(String URL)throws IOException{
 		String title="";
 		Document document = Jsoup.connect(URL).followRedirects(false).timeout(60000/*wait up to 60 sec for response*/).get();
@@ -56,20 +83,41 @@ public class DB {
 		title= td.text();
 		return title;
 	}
-	public void getLinks() throws IOException{
-		Document document = Jsoup.connect(this.url).followRedirects(false).timeout(60000/*wait up to 60 sec for response*/).get();
-		Elements links = document.getElementsByTag("a");
+	
+	public void getLinks(String url) throws IOException{
+		Document document = Jsoup.connect(url).followRedirects(false).timeout(60000/*wait up to 60 sec for response*/).get();
+		Element table = document.select("tabele").get(9);
+		Elements links = table.getElementsByTag("a");
 		int len = links.size();
 		for(int i=0;i<len;i++){
 			String link ="http://www.courts.ie"+ links.get(i).attr("href");
-			if(link.contains("OpenDocument")){
-				this.links.add(link);
-				System.out.println(link);
-			}
+			this.links.add(link);
 		}
-		System.out.println("Done getting links");
+		//System.out.println("Done getting links");
 	}
 
+	public void GenerateHTML(String table) throws IOException{
+		File file = new File("HTMLs/test.html");
+			
+			FileWriter fileWriter = null;
+			BufferedWriter bufferedWriter = null;
+			try {
+			fileWriter = new FileWriter(file);
+			bufferedWriter = new BufferedWriter(fileWriter);
+			String htmlPage = "<html><body style=’background-color:#ccc’><center>" + table + "</center></body></html>" ;
+			bufferedWriter.write(htmlPage);
+			}catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				}
+	}
+	public String getTableHTML(String URL)throws IOException{
+		Document document = Jsoup.connect(URL).followRedirects(false).timeout(60000/*wait up to 60 sec for response*/).get();
+		Element table = document.body().select("table").get(8);
+		String html = table.html();
+		return html;
+	}
+	
 	public String getTableText(String URL) throws IOException{
 		System.out.println("Getting text:\n");
 		Document document = Jsoup.connect(URL).followRedirects(false).timeout(60000/*wait up to 60 sec for response*/).get();
@@ -110,27 +158,23 @@ public class DB {
 	}
 	public int GetCounter(){return this.NOtexts;}
 	public HashMap<String , FVHashMap> getTextsVectors(){return freqvec;}
-	public static void main(String[] args) throws DocumentException {
+	public static void main(String[] args) throws DocumentException, IOException {
 
 		   PDFcreator creator = new PDFcreator();
-		   /*HashMap<String , FVHashMap> vector=null;
-		   try{
-			   temp.init();
-			   temp.collect();
-			   vector = temp.getTextsVectors();
-			   
-		   }catch(IOException e){System.out.println(e.toString());}
-		   System.out.println("Number of texts: " + temp.GetCounter());*/
-		   try {
-			   DB temp = new DB();
-			   String url = "http://www.courts.ie/Judgments.nsf/597645521f07ac9a80256ef30048ca52/8e8929c277c2fe5e8025811a0057b95a?OpenDocument";
-			   String title ="PDFs/" + temp.getTitle(url)+".pdf";
-			   creator.pdfcreate(temp.getTableText(url),title);
-			/*String str = temp.getTableText("http://www.courts.ie/Judgments.nsf/597645521f07ac9a80256ef30048ca52/8e8929c277c2fe5e8025811a0057b95a?OpenDocument");
-			System.out.println(str);*/
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		   //	   FileInputStream file = new FileInputStream("HTMLs/test.html");
+		  BufferedReader buff = new BufferedReader(new FileReader(new File("HTMLs/test.html")));
+		   String html="";
+		   String temp;
+		   while((temp=buff.readLine())!=null)
+			   html+=temp;
+		  creator.pdfconvertor(html);
+		  /* DB temp = new DB();
+		   String text = temp.getTableHTML("http://www.courts.ie/Judgments.nsf/597645521f07ac9a80256ef30048ca52/95044a0f312a3c388025811e003a218e?OpenDocument");
+		   temp.GenerateHTML(text);*/
+		  /* String url = "http://www.courts.ie/Judgments.nsf/597645521f07ac9a80256ef30048ca52/8e8929c277c2fe5e8025811a0057b95a?OpenDocument";
+		   String title ="PDFs/" + temp.getTitle(url)+".pdf";
+		   creator.pdfcreate(temp.getTableText(url),title);*/
+		/*String str = temp.getTableText("http://www.courts.ie/Judgments.nsf/597645521f07ac9a80256ef30048ca52/8e8929c277c2fe5e8025811a0057b95a?OpenDocument");
+		System.out.println(str);*/
 	    }
 }
--- DBPreProcessing/src/controller/PDFcreator.java
+++ DBPreProcessing/src/controller/PDFcreator.java
@@ -1,16 +1,28 @@
 package controller;
 
+import java.io.ByteArrayInputStream;
+
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+
+import java.nio.charset.Charset;
+
+
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Font;
+import com.itextpdf.text.PageSize;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.pdf.PdfWriter;
 
+import com.itextpdf.tool.xml.XMLWorkerHelper;
+
+
 public class PDFcreator {
 
-	private static String FILE = "PDFs/FirstPdf.pdf";
+	//private static String FILE = "PDFs/FirstPdf.pdf";
     private static Font subFont = new Font(Font.FontFamily.TIMES_ROMAN, 12);
 	public PDFcreator(){
 		
@@ -27,7 +39,51 @@ public class PDFcreator {
 	        } catch (Exception e) {
 	            e.printStackTrace();
 	        }
-		
 	}
+	
+	public void converttopdf(String html){	
+		try{
+		Document document = new Document();
+		PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream("PDFs/convert.pdf"));
+		document.open();
+		
+		XMLWorkerHelper.getInstance().parseXHtml(writer,document,new StringReader(html.replaceAll(" ", ""))); 
+		document.close();
+		System.out.println( "PDF Created!" );
+		}catch(Exception e){
+			 e.printStackTrace();
+		}
+	}
+	public void pdfconvertor(String html) throws IOException, DocumentException{
+		Document document = new Document(PageSize.LETTER);
+		PdfWriter pdfWriter = PdfWriter.getInstance(document, new FileOutputStream("PDFs/convert.pdf"));
+		document.open();
+
+
+		XMLWorkerHelper worker = XMLWorkerHelper.getInstance(); 
+
+		Reader htmlReader = new StringReader(html);
+
+		worker.parseXHtml(pdfWriter, document, htmlReader);
+		document.close();
+
+	}
+	public void convert(String html) throws DocumentException, IOException{
+		// step 1
+        Document document = new Document();
+        // step 2
+        PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream("PDFs/Convert.pdf"));
+        // step 3
+        document.open();
+        // step 4
+   
+ 
+        XMLWorkerHelper worker = XMLWorkerHelper.getInstance();
+        ByteArrayInputStream is = new ByteArrayInputStream(html.getBytes("UTF-8"));
+        worker.parseXHtml(writer, document, is, Charset.forName("UTF-8"));
+        // step 5
+        document.close();
+    }
 }
+
 
