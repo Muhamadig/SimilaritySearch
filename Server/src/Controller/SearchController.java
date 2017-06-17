740d9b5bca886271c41de86f6c1fd61b4f513929
Muhamad Igbaria <muhamadig.ac@gmail.com>
2017-06-18 00:08:34 +0300

    preparation to find the cluster and then run pareto is ready at View.Search.search

--- Server/src/Controller/SearchController.java
+++ Server/src/Controller/SearchController.java
@@ -1,7 +1,51 @@
 package Controller;
 
+import java.io.File;
+import java.util.Map.Entry;
+
+import model.FVHashMap;
+import model.FVKeySortedMap;
+import model.FVValueSorted;
+
 public class SearchController {
 	
 	
+	
+	
+	
+	private static FVHashMap reduceFV(FVHashMap fv, FVHashMap common) {
+		FVHashMap reducedFV=(FVHashMap) fv.clone();
+		for(String key:fv.keySet()){
+			if(common.containsKey(key)) reducedFV.remove(key);
+		}
+		return reducedFV;
+	}
 
+	
+	public static FVHashMap expandFV(FVHashMap initial,FVValueSorted global,FVValueSorted common){
+		
+		FVHashMap globalFV=new FVHashMap();
+		FVHashMap commonFV=new FVHashMap();
+		
+		for(Entry<String ,Integer> entry:global){
+			globalFV.put(entry.getKey(), entry.getValue());
+		}
+		
+		for(Entry<String ,Integer> entry:common){
+			commonFV.put(entry.getKey(), entry.getValue());
+		}
+		
+		//reducedGlobal=GLOBAL-COMMON;
+		FVHashMap reducedGlobal=reduceFV(globalFV,commonFV);
+		
+		//currFV=INITIAL-COMMON
+		FVHashMap currFV = reduceFV(initial,commonFV);
+		
+		for(String key:reducedGlobal.keySet()){
+			if(!currFV.containsKey(key)) currFV.put(key, 0);
+		}
+
+		return currFV;
+		
+	}
 }
--- Server/src/Views/Search.java
+++ Server/src/Views/Search.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import Controller.SearchController;
 import Utils.Request;
 import model.FVHashMap;
+import model.FVKeySortedMap;
 import model.FVValueSorted;
 
 public class Search extends View{
@@ -16,23 +17,31 @@ public class Search extends View{
 		
 		ArrayList<byte[]> texts=new ArrayList<>();
 		
-		//frequency vector after remove stop words and synonyms and stemming.
+		//Step 1: get the text initial frequency vector from the user 
+		//the initial frequency vector is filtered from stop words , synonyms and stemming.
 		FVHashMap textFV=(FVHashMap) request.getParam("fv");
 		
-		//get common words vector
+		//Step 2: get the database common words frequency vector.
 		FVValueSorted cWVector=globalDao.getVector("common");
-		//get global words vector
+		
+		//Step 3: get the database global frequency vector.
 		FVValueSorted globalVector=globalDao.getVector("global");
 
-		//expand initial FV -->with reduce 
+		//Step 4: remove the common words from the initial FV and then expand the vector.
+		FVHashMap expandedFV=SearchController.expandFV(textFV, globalVector, cWVector);
 		
-		//sort the expanded vector by key ==final Text vector
 		
-		//find the text cluster
+		//Step 5: Sort the expanded Vector By Key.
+		FVKeySortedMap finalFV=new FVKeySortedMap(expandedFV);
 		
-		//pareto
+		//Step 6: Find the cluster that the Final FV belongs to.
 		
-		//return the best 5 results
+		
+		
+		//Step 7: Run Pareto Algorithm To Find best 5 Texts.
+		
+		
+		//Step 8: return pareto results.
 		
 		return texts;
 
