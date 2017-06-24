b3321d40558411a82830d12bcd1f843c6d7bf205
abdalla srouji <abdalla944@gmail.com>
2017-06-24 04:28:29 +0300

    no message

--- DBPreProcessing/src/CWTest.java
+++ DBPreProcessing/src/CWTest.java
@@ -10,7 +10,7 @@ public class CWTest {
 		long t = System.currentTimeMillis();
 		Cluster clusters = new Cluster();
 		
-		clusters.findC_global_fv("Clusters_Global", "Clusters", "FinalFVs");
+		clusters.findC_global_fv("Clusters_Global", ".", "FinalFVs");
 		HashMap<Integer,String> thresholds= new HashMap<Integer,String>();
 		thresholds.put(0, "1996");
 		thresholds.put(1, "16");
@@ -18,7 +18,7 @@ public class CWTest {
 		thresholds.put(3, "protective");
 		thresholds.put(4, "barricade");
 		thresholds.put(5, "dec");
-		clusters.find_CW_Sig("Clusters_CW&SIG", "Clusters", "Clusters_Global", thresholds);
+		clusters.find_CW_Sig("Clusters_CW&SIG", ".", "Clusters_Global", thresholds);
 		System.out.println("Done..." + ((System.currentTimeMillis() - t)/1000)+" Seconds");
 	}
 
